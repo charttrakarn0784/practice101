fea96d51935a7e26687e7087a3b90652eb2966d5
charttrakarn0784 <charttrakarn6@hotmail.co.th>
2020-01-23 23:42:10 +0700

    prob_set_7

---
+++ prob_set7/prob7_1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char grade;
+    cout << "What's your grade?: ";
+    cin >> grade;
+    switch(grade)
+    {
+        case 'A':
+            cout << "Awesome";
+            break;
+        case 'B':
+            cout << "Great";
+            break;
+        case 'C':
+            cout << "Good";
+            break;
+        case 'F':
+            cout << "Noob";
+            break;
+        default:
+            cout << "Error: Invalid Input";
+    }
+
+    return 0;
+}
