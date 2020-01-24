16bacaeb1da280058be75a4eac2ae9ffbcf3f883
charttrakarn0784 <charttrakarn6@hotmail.co.th>
2020-01-24 21:15:41 +0700

    prob_set_8

---
+++ prob_set8/prob8_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    double x,sum=0;
+    for(int i = 2;i <= 222;i+=2)
+    {
+        sum += (1.0/i);
+    }
+    cout << fixed << setprecision(10);
+    cout << "And the answer is ===> " << sum;
+
+    return 0;
+}
