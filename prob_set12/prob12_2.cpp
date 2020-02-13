118ad34d74b95a26a1d39f2e2080c9a24c27ae7c
charttrakarn0784 <charttrakarn6@hotmail.co.th>
2020-02-13 22:01:49 +0700

    prob_set_12

---
+++ prob_set12/prob12_2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int N;
+    string s1 = "KAK";
+    string s2 = "KAM KAM";
+    string s3 = s2;
+    if(s1 == "KAK"){
+        N = s2.size();
+        if(s2[0] == 'K' && s2[N-1] == 'M'){
+            for(int i = 0; i<N;i++){
+                s3[i] = s2[N-i-1];
+            }
+        }
+    }
+    cout << s1+" "+s3 << "\n";
+    cout << N;
+    return 0;
+}
